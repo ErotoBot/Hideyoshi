7b113155e165d211e120db8cb3e5786a8f274676
noud02 <noud.kerver@hotmail.nl>
2018-03-18 11:39:14 +0100

    define post in Http

--- lib/Http.cpp
+++ lib/Http.cpp
@@ -16,6 +16,33 @@ namespace Hideyoshi {
             return nmemb;
         }
 
+        string * post(string url, map<string, string> headers, string body) {
+            cURLpp::Easy req;
+            string *res = new string();
+            list<string> header;
+
+            cURLpp::options::WriteFunctionCurlFunction cb(writer);
+            cURLpp::OptionTrait<void *, CURLOPT_WRITEDATA> data(res);
+            cURLpp::options::PostFields fields(body);
+            cURLpp::options::PostFieldSize fieldsLen(body.size());
+
+            req.setOpt<cURLpp::options::Url>(url);
+            req.setOpt(cb);
+            req.setOpt(data);
+            req.setOpt(fields);
+            req.setOpt(fieldsLen);
+
+            for (auto hdr : headers) {
+                header.push_back(hdr.first + ": " + hdr.second);
+            }
+
+            req.setOpt<cURLpp::options::HttpHeader>(header);
+
+            req.perform();
+
+            return res;
+        }
+
         string * get(string url, map<string, string> headers) {
             cURLpp::Easy req;
             string *body = new string();
